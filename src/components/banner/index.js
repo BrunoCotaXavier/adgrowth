704724305cd5cb74ec402960511e55c80db53ff3
BrunoCotaXavier <brunocota@hotmail.com.br>
2024-05-16 20:07:15 -0300

    fix/mudanca-btns

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,4 +1,3 @@
-import { BtnStart } from '../btnStart';
 import './banner-style.css'
 
 export const BannerComponent = () => {
@@ -12,8 +11,13 @@ export const BannerComponent = () => {
                         <h2 className="text-white max-w-400 sm:max-w-580 text-left text-sm"><span className='font-bold'>TORNE-SE O Nº1 NO GOOGLE</span> E AUMENTE SUA RELEVÂNCIA NAS REDES SOCIAIS </h2>
                     </div>
                 </div>
-                <div className="flex space-x-4 ">
-                    <BtnStart />
+                <div className="flex space-x-4">
+                    <a href='/#form'>
+                        <button className="text-white btnSuccess">INICIAR</button>
+                    </a>
+                    <a href='/#funil'>
+                        <button className="text-white btnAbout">LER SOBRE</button>
+                    </a>
                 </div>
             </div>
         </div>
